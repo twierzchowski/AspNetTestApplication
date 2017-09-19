b025a420c5ac1938a1b5b97a5460df2e1bbc2525
Tomasz Wierzchowski <twierzchowski@future-processing.com>
2017-09-19 19:45:05 +0200

    init commit

--- AspNetTestApplication/Controllers/HomeController.cs
+++ AspNetTestApplication/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace AspNetTestApplication.Controllers
     {
         public ActionResult Index()
         {
+
             return View();
         }
 
--- AspNetTestApplication/Global.asax.cs
+++ AspNetTestApplication/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,38 @@ namespace AspNetTestApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            using (var dbContext = new TestContext(connectionString))
+            {
+                dbContext.Database.Initialize(true);
+            }
+        }
+    }
+
+    public class Test
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class TestContext : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+        public TestContext(string connectionString) : base(connectionString)
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<DbContext>());
+        }
+    }
+
+
+    public class DbInitializer : DropCreateDatabaseIfModelChanges<TestContext>
+    {
+        protected override void Seed(TestContext context)
+        {
+            context.Tests.AddRange(new List<Test>{new Test{id = 1, name="test"}, new Test{id=2, name="ok"}});
+            context.SaveChanges();
         }
     }
 }
